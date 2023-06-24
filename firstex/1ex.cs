da73bdd05b3dad7587f2cb5426280d197d764735
Марина Гречко <marinagrecko@MacBook-Air-Marina.local>
2023-06-24 22:06:53 +0300

    first

---
+++ firstex/1ex.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+//int a = new Random().Next(1, 10);
+
+Console.WriteLine("Введите второе число");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) {
+    Console.WriteLine("max=");
+    Console.WriteLine(a);
+}
+else {
+    Console.WriteLine("max=");
+    Console.WriteLine(b);
+}
---
+++ fourthex/4ex.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count <= num1) {
+    if (count% 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+
+    
+    count = count + 1;
+}
